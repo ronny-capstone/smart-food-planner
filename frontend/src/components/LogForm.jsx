9aa07acc6fbd307e7f648e89b1710ce682ec8619
Ronny Erlich <ronnye@meta.com>
2025-06-30 15:30:01 -0700

    initialized update form

--- frontend/src/components/LogForm.jsx
+++ frontend/src/components/LogForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { API_BASE_URL } from "../utils/api";
-export default function LogForm({ handleLogAdded, setShowModal }) {
+export default function LogForm({ handleLogAdded, setShowModal, type }) {
   const [userId, setUserId] = useState();
   const [dateLogged, setDateLogged] = useState(
     new Date().toISOString().split("T")[0]
@@ -10,53 +10,87 @@ export default function LogForm({ handleLogAdded, setShowModal }) {
   const [servings, setServings] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (foodItem.trim() === "" || servings.trim() === "") {
-      alert("Please fill out all fields.");
-      return;
-    }
-    if (
-      !Number.isInteger(parseInt(foodItem)) ||
-      !Number.isInteger(parseInt(servings)) ||
-      parseInt(servings) <= 0
-    ) {
-      alert("Please enter a positive number for servings.");
-      return;
-    }
+    if (type === "add") {
+      e.preventDefault();
+      if (foodItem.trim() === "" || servings.trim() === "") {
+        alert("Please fill out all fields.");
+        return;
+      }
+      if (
+        !Number.isInteger(parseInt(foodItem)) ||
+        !Number.isInteger(parseInt(servings)) ||
+        parseInt(servings) <= 0
+      ) {
+        alert("Please enter a positive number for servings.");
+        return;
+      }
 
-    try {
-      await fetch(`${API_BASE_URL}/log`, {
-        method: "POST",
-        body: JSON.stringify({
-          user_id: 1,
-          date_logged: dateLogged,
-          item_id: parseInt(foodItem),
-          servings: parseInt(servings),
-        }),
-        headers: {
-          "Content-type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          handleLogAdded(data);
-          setDateLogged("");
-          setFoodItem("");
-          setServings("");
+      try {
+        await fetch(`${API_BASE_URL}/log`, {
+          method: "POST",
+          body: JSON.stringify({
+            user_id: 1,
+            date_logged: dateLogged,
+            item_id: parseInt(foodItem),
+            servings: parseInt(servings),
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
         })
-        .catch((err) => {
-          console.log("Failed to create log:", err);
-        });
-    } catch (err) {
-      console.log("Failed to create log:", err.message);
-    }
+          .then((response) => response.json())
+          .then((data) => {
+            handleLogAdded(data);
+            setDateLogged("");
+            setFoodItem("");
+            setServings("");
+          })
+          .catch((err) => {
+            console.log("Failed to create log:", err);
+          });
+      } catch (err) {
+        console.log("Failed to create log:", err.message);
+      }
 
-    setShowModal(false);
+      setShowModal(false);
+    } else if (type === "update") {
+      try {
+        await fetch(`${API_BASE_URL}/${logToUpdate.id}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            user_id: 1,
+            date_logged: dateLogged,
+            item_id: parseInt(foodItem),
+            servings: parseInt(servings),
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setDateLogged("");
+            setFoodItem("");
+            setServings("");
+          })
+          .catch((err) => {
+            console.log("Failed to update log:", err);
+          });
+      } catch (err) {
+        console.log("Failed to update log:", err.message);
+      }
+    }
   };
+
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <div className="flex flex-col items-center justify-start p-6 bg-gray-50">
-        <h1 className="text-xl font-bold mb-6">Add New Log</h1>
+        {type === "add" && (
+          <h1 className="text-xl font-bold mb-6">Add New Log</h1>
+        )}
+        {type === "update" && (
+          <h1 className="text-xl font-bold mb-6">Update Log</h1>
+        )}
 
         <div className="mb-4 w-full max-w-sm">
           <label className="block mb-1 font-medium">
