47243705561468c5cfe3c6df98479ef4a82850d8
Ronny Erlich <ronnye@meta.com>
2025-06-30 11:51:12 -0700

    Added log form and modal

---
+++ frontend/src/components/LogForm.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+export default function LogForm({ handleLogAdded, setShowModal }) {
+  const [userId, setUserId] = useState();
+  const [dateLogged, setDateLogged] = useState("");
+  const [foodItem, setFoodItem] = useState("");
+  const [servings, setServings] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      dateLogged.trim() === "" ||
+      foodItem.trim() === "" ||
+      servings.trim() === ""
+    ) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    try {
+      console.log(foodItem);
+      console.log(servings);
+
+      await fetch(`http://localhost:3000/log`, {
+        method: "POST",
+        body: JSON.stringify({
+          user_id: 1,
+          date_logged: dateLogged,
+          item_id: parseInt(foodItem),
+          servings: parseInt(servings),
+        }),
+        headers: {
+          "Content-type": "application/json;",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          handleLogAdded(data);
+          setDateLogged("");
+          setFoodItem("");
+          setServings("");
+        })
+        .catch((err) => {
+          console.log("Failed to create log:", err);
+        });
+    } catch (err) {
+      console.log("Failed to create log:", err.message);
+    }
+
+    setShowModal(false);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <div className="flex flex-col items-center justify-start p-6 bg-gray-50">
+        <h1 className="text-xl font-bold mb-6">Add New Log</h1>
+
+        <div className="mb-4 w-full max-w-sm">
+          <label className="block mb-1 font-medium">Date logged:</label>
+          <input
+            className="w-full border border-gray-400 rounded px-2 py-1 hover:bg-gray-100"
+            name="dateLogged"
+            value={dateLogged}
+            onChange={(e) => setDateLogged(e.target.value)}
+          />
+        </div>
+        <div className="mb-4 w-full max-w-sm">
+          <label className="block mb-1 font-medium">Food item:</label>
+          <input
+            className="w-full border border-gray-400 rounded px-2 py-1 hover:bg-gray-100"
+            name="foodItem"
+            value={foodItem}
+            onChange={(e) => setFoodItem(e.target.value)}
+          />
+        </div>
+        <div className="mb-4 w-full max-w-sm">
+          <label className="block mb-1 font-medium">Servings:</label>
+          <input
+            className="w-full border border-gray-400 rounded px-2 py-1 hover:bg-gray-100"
+            name="servings"
+            value={servings}
+            onChange={(e) => setServings(e.target.value)}
+          />
+        </div>
+        <div className="mb-1 w-full max-w-sm">
+          <button
+            type="submit"
+            className="px-2 py-1 rounded bg-green-100 hover:bg-green-200 transition mb-2"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
