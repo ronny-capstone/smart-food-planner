5e97027af626fbb6dda4d53560edabe358f94d73
Ronny Erlich <ronnye@meta.com>
2025-07-09 13:02:58 -0700

    Macro calculations

--- backend/routes/userProfileRoutes.js
+++ backend/routes/userProfileRoutes.js
@@ -3,13 +3,103 @@ const express = require("express");
 const path = require("path");
 const { STATUS_CODES } = require("http");
 const checkInvalidVariable = require("../utils/invalidVars.jsx");
-const { calculateBMR, calculateTDEE } = require("../utils/healthCalc.jsx");
+const {
+  calculateBMR,
+  calculateTDEE,
+  calculateMacroTargets,
+} = require("../utils/healthCalc.jsx");
 const StatusCodes = require("http-status-codes").StatusCodes;
 
 const profileRoutes = express.Router();
 const dbPath = path.resolve(__dirname, "../db/fridge.db");
 
 const db = new sqlite3.Database(dbPath);
+
+const updateProfile = (id, profileInfo, res) => {
+  const {
+    health_goal,
+    dietary_preferences,
+    age,
+    weight_kg,
+    height_feet,
+    height_inches,
+    gender,
+    activity,
+  } = profileInfo;
+
+  try {
+    // Update BMR, TDEE, and macro targets with new information
+    const updatedBmr = calculateBMR(
+      gender,
+      age,
+      weight_kg,
+      height_feet,
+      height_inches
+    );
+    const updatedTdee = calculateTDEE(
+      gender,
+      age,
+      activity,
+      weight_kg,
+      height_feet,
+      height_inches
+    );
+    const macroTargets = calculateMacroTargets(updatedTdee, health_goal);
+    db.run(
+      `UPDATE users SET health_goal = ?, dietary_preferences = ?, age = ?, weight_kg = ?, height_feet = ?, height_inches = ?, gender = ?, activity = ?, calorie_goal = ?, protein_goal = ?, carb_goal = ?, fat_goal = ?, bmr = ?, tdee = ?, profile_completed = ? WHERE id = ?`,
+      [
+        health_goal,
+        dietary_preferences,
+        age,
+        weight_kg,
+        height_feet,
+        height_inches,
+        gender,
+        activity,
+        macroTargets.calorie_goal,
+        macroTargets.protein_goal,
+        macroTargets.carb_goal,
+        macroTargets.fat_goal,
+        updatedBmr,
+        updatedTdee,
+        true,
+        id,
+      ],
+      function (err) {
+        if (err) {
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "An error occurred while updating the user profile",
+          });
+        }
+        return res.status(StatusCodes.OK).json({
+          message: "Profile updated successfully",
+          profile: {
+            id,
+            health_goal,
+            dietary_preferences,
+            age,
+            weight_kg,
+            height_feet,
+            height_inches,
+            gender,
+            activity,
+            calorie_goal: macroTargets.calorie_goal,
+            protein_goal: macroTargets.protein_goal,
+            carb_goal: macroTargets.carb_goal,
+            fat_goal: macroTargets.fat_goal,
+            bmr: updatedBmr,
+            tdee: updatedTdee,
+            profile_completed: true,
+          },
+        });
+      }
+    );
+  } catch (err) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error updating user profile" });
+  }
+};
 
 // Create/update user profile
 profileRoutes.post("/", (req, res) => {
@@ -49,7 +139,7 @@ profileRoutes.post("/", (req, res) => {
     height_feet,
     height_inches
   );
-  const macroTargets = calculateMacroTargets(tdee, health_goal, weight_kg);
+  const macroTargets = calculateMacroTargets(tdee, health_goal);
 
   // Check if user's profile already exists
   try {
@@ -64,44 +154,22 @@ profileRoutes.post("/", (req, res) => {
         }
         // If user's profile exists, update the profile
         if (row) {
-          try {
-            db.query(
-              `UPDATE users SET health_goal = ?, dietary_preferences = ?, age = ?, weight_kg = ?, height_feet = ?, height_inches = ?, gender = ?, activity = ?, bmr = ?, tdee = ?, profile_completed = true WHERE id = ?`,
-              [
-                health_goal,
-                dietary_preferences,
-                age,
-                weight_kg,
-                height_feet,
-                height_inches,
-                gender,
-                activity,
-                bmr,
-                tdee,
-                id,
-              ],
-              function (err) {
-                if (err) {
-                  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    message:
-                      "An error occurred while updating the user profile",
-                  });
-                }
-                return res
-                  .status(StatusCodes.OK)
-                  .json({ message: "Profile updated successfully" });
-              }
-            );
-          } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-              message: "An error occurred during profile update",
-            });
-          }
+          const profileInfo = {
+            health_goal,
+            dietary_preferences,
+            age,
+            weight_kg,
+            height_feet,
+            height_inches,
+            gender,
+            activity,
+          };
+          updateProfile(id, profileInfo, res);
         } else {
           // Create a new user profile
           try {
             db.run(
-              `INSERT INTO users (id, health_goal, dietary_preferences, age, weight_kg, height_feet, height_inches, gender, activity, bmr, tdee, profile_completed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+              `INSERT INTO users (id, health_goal, dietary_preferences, age, weight_kg, height_feet, height_inches, gender, activity, calorie_goal, protein_goal, carb_goal, fat_goal, bmr, tdee, profile_completed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
               [
                 id,
                 health_goal,
@@ -112,6 +180,10 @@ profileRoutes.post("/", (req, res) => {
                 height_inches,
                 gender,
                 activity,
+                macroTargets.calorie_goal,
+                macroTargets.protein_goal,
+                macroTargets.carb_goal,
+                macroTargets.fat_goal,
                 bmr,
                 tdee,
                 true,
@@ -156,8 +228,6 @@ profileRoutes.patch("/:id", (req, res) => {
       height_inches,
       gender,
       activity,
-      bmr,
-      tdee,
     } = req.body;
 
     if (
@@ -169,97 +239,45 @@ profileRoutes.patch("/:id", (req, res) => {
       checkInvalidVariable(height_feet) ||
       checkInvalidVariable(height_inches) ||
       checkInvalidVariable(gender) ||
-      checkInvalidVariable(activity) ||
-      checkInvalidVariable(bmr) ||
-      checkInvalidVariable(tdee)
+      checkInvalidVariable(activity)
     ) {
       return res
         .status(STATUS_CODES.BAD_REQUEST)
         .json({ message: "Missing required fields" });
     }
 
-    // Update BMR and TDEE with new information
-    const updatedBmr = calculateBMR(
-      gender,
-      age,
-      weight_kg,
-      height_feet,
-      height_inches
-    );
-    const updatedTdee = calculateTDEE(
-      gender,
-      age,
-      activity,
-      weight_kg,
-      height_feet,
-      height_inches
-    );
-    const macroTargets = calculateMacroTargets(tdee, health_goal, weight_kg);
-
-    try {
-      db.get(
-        "SELECT profile_completed FROM users WHERE id = ?",
-        [id],
-        async (err, row) => {
-          if (err) {
-            return res
-              .status(StatusCodes.INTERNAL_SERVER_ERROR)
-              .json({ message: "Error fetching user profile" });
-          }
-          if (!row) {
-            return res
-              .status(StatusCodes.NOT_FOUND)
-              .json({ message: "User not found" });
-          }
-
-          try {
-            db.query(
-              `UPDATE users SET health_goal = ?, dietary_preferences = ?, age = ?, weight_kg = ?, height_feet = ?, height_inches = ?, gender = ?, activity = ?, calorie_goal = ?, protein_goal = ?, carb_goal = ?, fat_goal = ?, bmr = ?, tdee = ?, profile_completed = true WHERE id = ?`,
-              [
-                health_goal,
-                dietary_preferences,
-                age,
-                weight_kg,
-                height_feet,
-                height_inches,
-                gender,
-                activity,
-                calorie_goal,
-                protein_goal,
-                carb_goal,
-                fat_goal,
-                updatedBmr,
-                updatedTdee,
-                id,
-              ],
-              function (err) {
-                if (err) {
-                  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                    message:
-                      "An error occurred while updating the user profile",
-                  });
-                }
-                return res
-                  .status(StatusCodes.OK)
-                  .json({ message: "Profile updated successfully" });
-              }
-            );
-          } catch (err) {
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-              message: "An error occurred during profile update",
-            });
-          }
+    db.get(
+      "SELECT profile_completed FROM users WHERE id = ?",
+      [id],
+      async (err, row) => {
+        if (err) {
+          return res
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json({ message: "Error fetching user profile" });
         }
-      );
-    } catch (err) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        message: "An error occurred while updating grocery item",
-      });
-    }
+        if (!row) {
+          return res
+            .status(StatusCodes.NOT_FOUND)
+            .json({ message: "User not found" });
+        }
+
+        const profileInfo = {
+          health_goal,
+          dietary_preferences,
+          age,
+          weight_kg,
+          height_feet,
+          height_inches,
+          gender,
+          activity,
+        };
+        updateProfile(id, profileInfo, res);
+      }
+    );
   } catch (err) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: "An error occurred in the profile update route",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error updating user profile" });
   }
 });
 
